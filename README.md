f8525a436028bee2dadc6a9c70bf289cd7ac4406
Rudhra Raveendran <rudytheninja@gmail.com>
2017-04-19 23:19:46 -0400

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Chatbot-Workshop
-Code for April 2017 MSP Workshop
+This is the code for the April 2017 BU MSP Workshop.
+
+In this workshop, we go over creating a basic chatbot using the Microsoft Bot Framework and Recast.AI.
+The bot can be accessed through Facebook Messenger. This branch of the code contains the basic version of the bot.
--- bot.js
+++ bot.js
@@ -1,30 +1,35 @@
+// Import the necessary modules
 var restify = require('restify');
 var builder = require('botbuilder');
 var recast = require('recastai');
-var recastClient = new recast.request('8836e21cb89a45d0f20d62ac3a5d2f0c', 'en');
-var server = restify.createServer().listen(8080);
 
+// Initialize Recast.AI and the Microsoft Bot Framework
+// Make sure to use your own tokens/IDs/passwords etc.
+var recastClient = new recast.request('8836e21cb89a45d0f20d62ac3a5d2f0c', 'en');
 var connector = new builder.ChatConnector({
 	appId: 'cec04ad8-2a68-4c7b-b31f-41c394b1e635',
 	appPassword: 'VSfWY6n4vuiWqeNVHmTt8xi'
 });
 
+// Initialize the bot
 var bot = new builder.UniversalBot(connector);
 
+// Set the behavior when a request is sent '/'
 bot.dialog('/', function(session) {
-	console.log(session.message.text);
-	recastClient.analyseText(session.message.text)
+	console.log(session.message.text); // Display the message we received in the console
+	recastClient.analyseText(session.message.text) // Have Recast analyze the message
 	.then(function(res) {
-		if (res.intent()) { console.log('Intent: ', res.intent().slug) }
-		if (res.intent().slug === 'greetings') {
+		if (res.intent()) { console.log('Intent: ', res.intent().slug) } // If an intent is discovered, display it
+		if (res.intent().slug === 'greetings') { // Do a specific action for a specific intent
 		  session.send("Hi there!");
 		}
 	})
-	.catch(function() {
+	.catch(function() { // Send a basic response if an error occurs
 		session.send('I need some sleep right now... Talk to me later!')
 	});
 });
 
+// Initialize the server to host the bot
 var server = restify.createServer();
 server.listen(8000);
 server.post('/', connector.listen());
